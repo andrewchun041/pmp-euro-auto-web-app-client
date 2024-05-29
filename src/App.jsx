a2220b6ffe75a4d3e169e212a66e833d69d1d5b0
Andrew SungWoo Chun <andrewchun041@gmail.com>
2024-05-29 10:29:58 -0700

    run npx create-react-app; delete unused files; modify js to jsx; npm i axios & react-router-dom; set up boilerplate BrowserRouter with Routes and default Route tags

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          {/* <Route path='/' element={< />}></Route> */}
+          {/* <Route path='/' element={< />}></Route> */}
+          {/* <Route path='/' element={< />}></Route> */}
+          {/* <Route path='/' element={< />}></Route> */}
+          {/* <Route path='/' element={< />}></Route> */}
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
