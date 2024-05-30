0c1338810400143b4a3aad17c723dad275b57d0d
Andrew SungWoo Chun <andrewchun041@gmail.com>
2024-05-29 17:24:22 -0700

    add navlink titles and align them to baseline

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,7 +7,12 @@ function Header () {
         <header className='header'>
             <Link className='header__logo-link logo' to='/'>PMP EURO AUTO</Link>
             <nav className='header__nav'>
-                <NavLink className='header__navlink'></NavLink>
+                <NavLink className='header__navlink' to='/'>Home</NavLink>
+                <NavLink className='header__navlink' to='/'>Vehicles</NavLink>
+                <NavLink className='header__navlink' to='/'>Parts Inventory</NavLink>
+                <NavLink className='header__navlink' to='/'>eBay Store</NavLink>
+                <NavLink className='header__navlink' to='/'>About</NavLink>
+                <NavLink className='header__navlink' to='/'>Contact</NavLink>
             </nav>
         </header>
     );
