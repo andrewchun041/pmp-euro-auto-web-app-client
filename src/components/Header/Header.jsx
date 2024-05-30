48c37994f92c226808e8b7a523d18633e6062a0a
Andrew SungWoo Chun <andrewchun041@gmail.com>
2024-05-30 10:13:02 -0700

    complete prototype header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,49 @@
-import './Header.scss';
+import "./Header.scss";
 
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink } from "react-router-dom";
 
-function Header () {
-    return (
-        <header className='header'>
-            <Link className='header__logo-link logo' to='/'>PMP EURO AUTO</Link>
-            <nav className='header__nav'>
-                <NavLink className='header__navlink' to='/'>Home</NavLink>
-                <NavLink className='header__navlink' to='/'>Vehicles</NavLink>
-                <NavLink className='header__navlink' to='/'>Parts Inventory</NavLink>
-                <NavLink className='header__navlink' to='/'>eBay Store</NavLink>
-                <NavLink className='header__navlink' to='/'>About</NavLink>
-                <NavLink className='header__navlink' to='/'>Contact</NavLink>
-            </nav>
-        </header>
-    );
+function Header() {
+  return (
+    <header className="header">
+      <Link className="header__logo-link logo" to="/">
+        PMP EURO AUTO
+      </Link>
+      <nav className="header__nav">
+        <ul className="header__navlist">
+          <li className="header__navlist-item">
+            <NavLink className="header__navlink" to="/">
+              Home
+            </NavLink>
+          </li>
+          <li className="header__navlist-item">
+            <NavLink className="header__navlink" to="/">
+              Vehicles
+            </NavLink>
+          </li>
+          <li className="header__navlist-item">
+            <NavLink className="header__navlink" to="/">
+              Parts
+            </NavLink>
+          </li>
+          <li className="header__navlist-item">
+            <NavLink className="header__navlink" to="/">
+              eBay Store
+            </NavLink>
+          </li>
+          <li className="header__navlist-item">
+            <NavLink className="header__navlink" to="/">
+              About
+            </NavLink>
+          </li>
+          <li className="header__navlist-item">
+            <NavLink className="header__navlink" to="/">
+              Contact
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
