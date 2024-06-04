b17eec4eccf79b91a8a4d8ac8af79660cc72c235
Andrew SungWoo Chun <andrewchun041@gmail.com>
2024-06-04 14:47:32 -0700

    remove empty ruleset in Header.scss; format Home.jsx and remove word 'photo' from img alt to reduce redundancy

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,32 +1,33 @@
-import './Home.scss';
-import galleryImage from '../../assets/images/home-page-gallery-photo.jpeg';
+import "./Home.scss";
+import galleryImage from "../../assets/images/home-page-gallery-photo.jpeg";
 
 function Home() {
   return (
     <>
-      <section className='hero-banner'>
-        <div className='hero-banner__background'>
-            <div className='hero-banner__text-container'>
-                <p className='hero-banner__slogan'>
-                    Recycle, Reuse, Save Money
-                </p>
-                <h1 className='hero-banner__title'>
-                    PMP EURO AUTO
-                </h1>
-                <p className='hero-banner__description'>
-                    We are your one stop for quality used OEM & Aftermarket parts for a wide range of European & JDM makes & models
-                </p>
-            </div>
+      <section className="hero-banner">
+        <div className="hero-banner__background">
+          <div className="hero-banner__text-container">
+            <p className="hero-banner__slogan">Recycle, Reuse, Save Money</p>
+            <h1 className="hero-banner__title">PMP EURO AUTO</h1>
+            <p className="hero-banner__description">
+              We are your one stop for quality used OEM & Aftermarket parts for
+              a wide range of European & JDM makes & models
+            </p>
+          </div>
         </div>
       </section>
-      <section className='gallery'>
-        <h2 className='gallery__title'>Gallery</h2>
-        <div className='gallery__img-container'>
-            <img className='gallery__img' src={galleryImage} alt="gallery photo" />
+      <section className="gallery">
+        <h2 className="gallery__title">Gallery</h2>
+        <div className="gallery__img-container">
+          <img
+            className="gallery__img"
+            src={galleryImage}
+            alt="gallery"
+          />
         </div>
       </section>
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
