b8357060148d4669a22695cff862816abf93c0f5
Andrew SungWoo Chun <andrewchun041@gmail.com>
2024-06-03 23:20:10 -0700

    add Home component for home page; add some site images; complete general layout of hero banner with styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import Home from './components/Home/Home';
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       <BrowserRouter>
         <Header />
         <Routes>
-          {/* <Route path='/' element={< />}></Route> */}
+          <Route path='/' element={<Home />}></Route>
           {/* <Route path='/' element={< />}></Route> */}
           {/* <Route path='/' element={< />}></Route> */}
           {/* <Route path='/' element={< />}></Route> */}
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,28 @@
+import './Home.scss'
+
+function Home() {
+  return (
+    <>
+      <section className='hero-banner'>
+        <div className='hero-banner__background'>
+            <div className='hero-banner__text-container'>
+                <p className='hero-banner__slogan'>
+                    Recycle, Reuse, Save Money
+                </p>
+                <h1 className='hero-banner__title'>
+                    PMP EURO AUTO
+                </h1>
+                <p className='hero-banner__description'>
+                    We are your one stop for quality used OEM & Aftermarket parts for a wide range of European & JDM makes & models
+                </p>
+            </div>
+        </div>
+      </section>
+      <section className='gallery'>
+
+      </section>
+    </>
+  )
+}
+
+export default Home
