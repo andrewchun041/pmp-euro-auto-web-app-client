58064f2b26ced7af0e4ecb5428153fa345e53e5f
Andrew SungWoo Chun <andrewchun041@gmail.com>
2024-06-06 15:03:21 -0700

    rename section with className vehicles to vehicles-interface and do the same for all subsequent BEM structures and scss; change search-input tablet length to be fixed length

--- src/components/VehiclesList/VehiclesList.jsx
+++ src/components/VehiclesList/VehiclesList.jsx
@@ -10,37 +10,40 @@ function VehiclesList() {
                 </div>
             </div>
         </section>
-        <section className="vehicles">
-            <div className="vehicles__interface">
-                <input className="vehicles__search-input" type="search" />
-                <div className="vehicles__filter-sort-num-prod-container">
-                    <div className="vehicles__filter-sort-container">
-                        <fieldset className="vehicles__filter-fieldset">
-                            <legend className="vehicles__filter-legend">Filter:</legend>
-                            <div className="vehicles__filter-select-container">
-                                <select className="vehicles__filter-select" name="make" id="make">
-                                    <option className="vehicles__filter-option" value="all">Make</option>
+        <section className="vehicles-interface">
+            <div className="vehicles-interface__container">
+                <input className="vehicles-interface__search-input" type="search" />
+                <div className="vehicles-interface__filter-sort-num-prod-container">
+                    <div className="vehicles-interface__filter-sort-container">
+                        <fieldset className="vehicles-interface__filter-fieldset">
+                            <legend className="vehicles-interface__filter-legend">Filter:</legend>
+                            <div className="vehicles-interface__filter-select-container">
+                                <select className="vehicles-interface__filter-select" name="make" id="make">
+                                    <option className="vehicles-interface__filter-option" value="all">Make</option>
                                 </select>
-                                <select className="vehicles__filter-select" name="model" id="model">
-                                    <option className="vehicles__filter-option" value="all">Model</option>
+                                <select className="vehicles-interface__filter-select" name="model" id="model">
+                                    <option className="vehicles-interface__filter-option" value="all">Model</option>
                                 </select>
                             </div>
                         </fieldset>
-                        <div className="vehicles__sort-container">
-                            <label className="vehicles__sort-label" htmlFor="sort">Sort:</label>
-                            <select className="vehicles__sort-select" name="sort" id="sort">
-                                <option className="vehicles__sort-option" value="new">New</option>
-                                <option className="vehicles__sort-option" value="old">Old</option>
-                                <option className="vehicles__sort-option" value="az">A-Z</option>
-                                <option className="vehicles__sort-option" value="za">Z-A</option>
+                        <div className="vehicles-interface__sort-container">
+                            <label className="vehicles-interface__sort-label" htmlFor="sort">Sort:</label>
+                            <select className="vehicles-interface__sort-select" name="sort" id="sort">
+                                <option className="vehicles-interface__sort-option" value="new">New</option>
+                                <option className="vehicles-interface__sort-option" value="old">Old</option>
+                                <option className="vehicles-interface__sort-option" value="az">A-Z</option>
+                                <option className="vehicles-interface__sort-option" value="za">Z-A</option>
                             </select>
                         </div>
                     </div>
-                    <div className="vehicles__num-prod-container">
-                        <p className="vehicles__num-prod-text"># products</p>
+                    <div className="vehicles-interface__num-prod-container">
+                        <p className="vehicles-interface__num-prod-text"># products</p>
                     </div>
                 </div>
             </div>
+        </section>
+        <section className="">
+            <article></article>
         </section>
     </>
   );
